64ce462f5b3eca0be7ca7b46a7154bcc70bafcb4
sudoajay <sudoajay@gmail.com>
2020-04-16 21:11:57 +0530

    Setup Dns  Widget

---
+++ app/src/main/java/com/sudoajay/dnswidget/ui/home/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.sudoajay.dnswidget.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.support.v4.app.Fragment
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import com.sudoajay.dnswidget.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
