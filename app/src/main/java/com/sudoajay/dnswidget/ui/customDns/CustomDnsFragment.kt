51ae068ef750d28eb79390fdd0945e6f2e673462
sudoajay <sudoajay@gmail.com>
2020-04-27 14:03:29 +0530

    Added Xml image of empty list

--- app/src/main/java/com/sudoajay/dnswidget/ui/customDns/CustomDnsFragment.kt
+++ app/src/main/java/com/sudoajay/dnswidget/ui/customDns/CustomDnsFragment.kt
@@ -1,22 +1,20 @@
 package com.sudoajay.dnswidget.ui.customDns
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.sudoajay.dnswidget.R
 
 class CustomDnsFragment : Fragment() {
 
     private lateinit var customDnsViewModel: CustomDnsViewModel
-    private lateinit var bottomSheetDialog: BottomSheetDialog
     private lateinit var root: View
 
     override fun onCreateView(
@@ -33,7 +31,6 @@ class CustomDnsFragment : Fragment() {
         return root
     }
 
-    @SuppressLint("InflateParams")
     private fun reference() {
 
         root.findViewById<FloatingActionButton>(R.id.addCustomDns_FloatingActionButton).setOnClickListener {
@@ -42,29 +39,16 @@ class CustomDnsFragment : Fragment() {
         val recycleView: RecyclerView = root.findViewById(R.id.recycler_view)
 
 
-//        bottomSheetDialog = BottomSheetDialog(requireContext())
-//        val sheetView = layoutInflater.inflate(R.layout.layout_bottomsheet_moreoption, null)
-//        sheetView.findViewById<LinearLayout>(R.id.share_BottomSheetDialog).setOnClickListener{
-//            bottomSheetDialog.dismiss()
-//        }
-//        sheetView.findViewById<LinearLayout>(R.id.edit_BottomSheetDialog).setOnClickListener{
-//            bottomSheetDialog.dismiss()
-//        }
-//        sheetView.findViewById<LinearLayout>(R.id.delete_BottomSheetDialog).setOnClickListener{
-//            bottomSheetDialog.dismiss()
-//        }
-//        sheetView.findViewById<LinearLayout>(R.id.copy_BottomSheetDialog).setOnClickListener{
-//            bottomSheetDialog.dismiss()
-//        }
-
         // Loads animals into the ArrayList
         customDnsViewModel.addAnimals()
 
+        if (customDnsViewModel.animals.isEmpty()) {
+            recycleView.visibility = View.GONE
+            root.findViewById<ImageView>(R.id.empty_imageView).visibility = View.VISIBLE
+        }
+
         // Creates a vertical Layout Manager
         recycleView.layoutManager = LinearLayoutManager(requireContext())
-
-        // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
-//        rv_animal_list.layoutManager = GridLayoutManager(this, 2)
 
         // Access the RecyclerView Adapter and load the data into it
         recycleView.adapter = CustomDnsAdapter(customDnsViewModel.animals, this)
